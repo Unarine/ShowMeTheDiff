1e9ddbb607b3372d3732094c5cda7457f8169cca
g12n1260 <g12n1260@G12N1260-1>
2016-10-02 14:39:09 +0200

    Add project files.

---
+++ ShowMeTheDiff/ShowMeTheDiff.cs
@@ -0,0 +1,177 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ShowMeTheDiff.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE80;
+using EnvDTE;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Editor;
+using System.IO;
+
+namespace ShowMeTheDiff
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class ShowMeTheDiff
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("508596cf-e79a-4766-9042-851a7f1aa5c1");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShowMeTheDiff"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private ShowMeTheDiff(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static ShowMeTheDiff Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new ShowMeTheDiff(package);
+        }
+
+        private IWpfTextViewHost GetCurrentViewHost() {
+            var textManager = this.ServiceProvider.GetService(typeof(SVsTextManager)) as IVsTextManager;
+            IVsTextView textView = null;
+            int mustHaveFocus = 1;
+            textManager.GetActiveView(mustHaveFocus, null, out textView);
+
+            var userData = textView as IVsUserData;
+            if (userData == null)
+            {
+                return null;
+            }
+            else {
+                Guid guidViewHost = DefGuidList.guidIWpfTextViewHost;
+                object holder;
+                userData.GetData(ref guidViewHost, out holder);
+                var viewHost = (IWpfTextViewHost)holder;
+                return viewHost;
+            }
+
+        }
+
+
+        private string GetAllText(IWpfTextViewHost viewHost) =>
+            viewHost.TextView.TextSnapshot.GetText();
+
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        ///// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+
+            var viewhost = GetCurrentViewHost();
+            var screengrab = GetAllText(viewhost);
+           
+            var dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+            var lol = dte.MainWindow.Document;
+            
+            string filename = dte.ActiveDocument.FullName;
+            string [] file_name = filename.Split('\\') ;
+            string directory = "";
+            for (int i = 0; i < file_name.Length-1; i++) {
+                directory += file_name[i] + "\\";
+            }
+
+            string[] FName = file_name[file_name.Length - 1].Split('.');
+            DateTime date = DateTime.Now;
+
+            string toWriteinto = FName[0] + "@" + date.Day + "_" + date.Month + "_" + date.Year + "_" + "@" + date.Hour + "h" + date.Minute + "." + FName[1] ;
+            
+
+            TextWriter txtResult = File.CreateText(directory+ toWriteinto);
+            
+            txtResult.Write(screengrab);
+            txtResult.Close();
+
+            
+            
+            //string file2 = "\"" + directory+ toWriteinto + "\"";
+
+            string file1, file2;
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = directory;
+            dialog.ShowDialog();
+
+            file1 = "\"" + dialog.FileName + "\"";
+            file2 = "\"" + filename + "\"";
+
+            // store this filename as something that UseThisLineInstead can use.
+            WorkingFile = filename;
+
+            dte.ExecuteCommand("Tools.Difffiles", $"{file1} {file2}");
+
+
+
+        }
+
+        public string WorkingFile { get; private set; }
+    }
+}
---
+++ ShowMeTheDiff/UseThisLineInstead.cs
@@ -0,0 +1,161 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="UseThisLineInstead.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Editor;
+using EnvDTE80;
+using EnvDTE;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text;
+
+namespace ShowMeTheDiff
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class UseThisLineInstead
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 256;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("aa89b473-06ee-4bbd-8c0d-a3722431334b");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UseThisLineInstead"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private UseThisLineInstead(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static UseThisLineInstead Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new UseThisLineInstead(package);
+        }
+
+
+        private IWpfTextViewHost GetCurrentViewHost()
+        {
+            var textManager = this.ServiceProvider.GetService(typeof(SVsTextManager)) as IVsTextManager;
+            IVsTextView textView = null;
+            int mustHaveFocus = 1;
+            textManager.GetActiveView(mustHaveFocus, null, out textView);
+
+            var userData = textView as IVsUserData;
+            if (userData == null)
+            {
+                return null;
+            }
+            else
+            {
+                Guid guidViewHost = DefGuidList.guidIWpfTextViewHost;
+                object holder;
+                userData.GetData(ref guidViewHost, out holder);
+                var viewHost = (IWpfTextViewHost)holder;
+                return viewHost;
+            }
+
+        }
+
+
+        private string GetAllText(IWpfTextViewHost viewHost) =>
+           // viewHost.TextView.Caret.ContainingTextViewLine;
+            //viewHost.TextView.GetTextViewLineContainingBufferPosition(viewHost.TextView.Caret.Position); 
+            viewHost.TextView.TextSnapshot.GetText();
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var viewhost = GetCurrentViewHost();
+            var line = viewhost.TextView.Caret.ContainingTextViewLine.Extent.GetText();
+            var position = viewhost.TextView.Caret.Position.BufferPosition.Position;
+
+
+            //TextExtent extent = new TextExtent();
+            //SnapshotSpan range = new SnapshotSpan() ;
+            //ITrackingSpan trackingSpan = range.Snapshot.CreateTrackingSpan(extent.Span, SpanTrackingMode.EdgeInclusive);
+            //ITextSnapshot m_snapshot = trackingSpan.TextBuffer.CurrentSnapshot;
+            //var curline = trackingSpan.GetText(m_snapshot);
+            //trackingSpan.TextBuffer.Replace(trackingSpan.GetSpan(m_snapshot), line);
+
+
+            //var dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+            //var lol = dte.MainWindow.Document;
+
+            /*string title = "UseThisLineInstead";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST); */
+        }
+    }
+}
